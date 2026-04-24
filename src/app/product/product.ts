cf28dbc2e6b43f2190b8508be66695acf58363b6
Salma Majri <salmamajri7@gmail.com>
2026-04-24 17:23:54 +0100

    refactor: implement OnInit interface in Product component

--- src/app/product/product.ts
+++ src/app/product/product.ts
@@ -1,17 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product',
+  standalone: true,
   imports: [CommonModule],
   templateUrl: './product.html',
-  styleUrl: './product.css',
-  standalone: true
+  styleUrl: './product.css'
 })
-export class Product {
-  products = [
-    {id: 1, name: "PC", price: 2900, selected: true},
-    {id: 2, name: "Tablet", price: 3600, selected: false},
-    {id: 3, name: "Printer", price: 3400, selected: true}
-  ];
+export class Product implements OnInit {
+  products: any[] = [];
+
+  ngOnInit(): void {
+    this.products = [
+      {id: 1, name: "PC", price: 2900, selected: true},
+          {id: 2, name: "Tablet", price: 3600, selected: false},
+          {id: 3, name: "Printer", price: 3400, selected: true}
+    ];
+  }
 }
