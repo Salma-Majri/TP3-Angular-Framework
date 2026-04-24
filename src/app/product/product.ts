b02c23907b549f0896b4d3154075e4656209af9f
Salma Majri <salmamajri7@gmail.com>
2026-04-24 17:08:20 +0100

    feat: implement product list

--- src/app/product/product.ts
+++ src/app/product/product.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
 @Component({
   selector: 'app-product',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product.html',
   styleUrl: './product.css',
   standalone: true
 })
-export class Product {}
+export class Product {
+  products = [
+    {id: 1, name: "PC", price: 2900, selected: true},
+    {id: 2, name: "Tablet", price: 3600, selected: false},
+    {id: 3, name: "Printer", price: 3400, selected: true}
+  ];
+}
